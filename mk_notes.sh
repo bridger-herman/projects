83712fc3dc993b401c5ffe08b0035dbd1905d441
Bridger Herman <scooteeroo@gmail.com>
2017-03-09 17:24:59 +1300

    Use vim instead of Atom to take notes, and fix earlier mistake

--- mk_notes.sh
+++ mk_notes.sh
@@ -1,7 +1,8 @@
 D=$(date +%m-%d-%Y.md)
-atom -w $D
-#~/GitHub/projects/./notes_cat.py
+# atom -w $D
+vim $D
+~/GitHub/projects/./notes_cat.py
 git add *.md
 git commit -m "Add $D"
-~/GitHub/projects/./gits.sh
+# ~/GitHub/projects/./gits.sh
 git push
