f4b88374270a838f204695d56a135f7b28c63933
Bridger Herman <scooteeroo@gmail.com>
2018-03-07 21:06:20 -0600

    Add robustness to todos

--- todos.py
+++ todos.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+import sys
 from finder import search_dir
 
 def display(line, number, filepath):
     print('{:03}  {}\n    {}'.format(number, filepath, line))
 
 def main():
-    infos = search_dir('TODO')
+    search_term = 'TODO'
+    if len(sys.argv) == 1:
+        infos = search_dir(search_term)
+    elif len(sys.argv) > 1:
+        infos = search_dir(search_term, sys.argv[1])
+    else:
+        infos = [(None, None, None)]
     for line, number, filepath in infos:
         display(line, number, filepath)
 
