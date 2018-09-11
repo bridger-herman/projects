7eec23aa7edf735757bf1405fadde033cbde1cdd
Bridger Herman <herma582@umn.edu>
2018-09-11 08:23:06 -0500

    Fix amendment of notes

--- mk_notes.sh
+++ mk_notes.sh
@@ -3,39 +3,41 @@ fix_date=""
 amend=0
 
 # Gather arguments
-while getopts "afx:" opt; do
+while getopts ":a:f:" opt; do
     case "$opt" in
-      a) amend=1;;
-      f) fix_date=$OPTARG;;
+        a) amend=1;;
+        f) fix_date=$OPTARG;;
     esac
 done
 
 # Create the necessary directories
 for d in src tmp tex pdf
 do
-  if [ \! -d $d ]
-  then
-    mkdir -p $d
-  fi
+    if [ \! -d $d ]
+    then
+        mkdir -p $d
+    fi
 done
 
-name=$(date +%Y-%d-%m)
-fancy_date=$(date +"%A, %B %d, %Y")
-work_dir=${PWD##*/}
-modified_files=""
-
-# Only amend the notes from today - don't create a new file
-if [ $amend -eq 0 ]
-then
-  echo "% Notes from $name" >> src/$name.md
-  echo "% Bridger Herman" >> src/$name.md
-  echo "% $fancy_date" >> src/$name.md
-fi
 
 # Go back and fix notes from a particular day
 if [[ $fix_date != "" ]]
 then
-  name=$fix_date
+    name=$fix_date
+else
+    name=$(date +%Y-%m-%d)
+fi
+
+fancy_date=$(date +"%A, %B %d, %Y" -d $name)
+work_dir=${PWD##*/}
+modified_files=""
+
+# Only create the new file if it doesn't already exist
+if [ ! -f src/$name.md ]
+then
+    echo "% Notes from $name" >> src/$name.md
+    echo "% Bridger Herman" >> src/$name.md
+    echo "% $fancy_date" >> src/$name.md
 fi
 
 # Edit the notes
@@ -46,8 +48,8 @@ modified_files+="src/$name.md"
 rm -f src/notes.md
 for f in $(ls src)
 do
-  printf "\n\n---\n" >> tmp/notes.md
-  cat src/$f >> tmp/notes.md
+    printf "\n\n---\n" >> tmp/notes.md
+    cat src/$f >> tmp/notes.md
 done
 cp tmp/notes.md src/notes.md
 modified_files+=" src/notes.md"
@@ -82,16 +84,16 @@ read -p "Look good? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  push_opts=""
+    push_opts=""
 
-  if [ $amend -eq 1 ]
-  then
-    git commit --amend
-    push_opts="-f"
-  else
-    git commit -m "Update notes for $name"
-  fi
+    if [ $amend -eq 1 ]
+    then
+        git commit --amend
+        push_opts="-f"
+    else
+        git commit -m "Update notes for $name"
+    fi
 
-  git push $push_opts
+    git push $push_opts
 fi
 
